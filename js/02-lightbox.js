b3396ba509b01b00126f577bd7afe49c28fa18a8
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-18 15:43:00 +0100

    Task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageGallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const modalLink = document.createElement("a");
+  modalLink.classList.add("gallery__item");
+  modalLink.setAttribute("href", item.original);
+
+  const galleryItem = document.createElement("img");
+  galleryItem.src = item.preview;
+  galleryItem.classList.add("gallery__image");
+  galleryItem.setAttribute("alt", item.description);
+
+  imageGallery.appendChild(modalLink);
+  modalLink.appendChild(galleryItem);
+});
+
+let lightbox = new SimpleLightbox(`.gallery a`, { captionSelector: "img", captionsData: "alt", captionDelay: 250, scrollZoom: false, animationSpeed: 350 });
+
+const modal = (e) => {
+  e.preventDefault();
+  e.lightbox.open();
+};
+
+imageGallery.addEventListener("click", modal);
